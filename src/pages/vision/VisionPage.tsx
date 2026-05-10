a9b074367dff27c2abc5b17ff39d7911821ab711
Petros Chantzopoulos <petros.chantz@gmail.com>
2026-05-11 00:29:54 +0200

    feat: preload group image and enhance VisionPage layout

--- src/pages/vision/VisionPage.tsx
+++ src/pages/vision/VisionPage.tsx
@@ -1,5 +1,6 @@
 import { Seo } from "../../seo/Seo";
 import { SITE_URL } from "../../lib/config";
+import { Helmet } from "react-helmet-async";
 import {
   bodyTextClass,
   introTextClass,
@@ -21,11 +22,19 @@ export function VisionPage() {
         canonical={canonical}
         ogImage={ogImage}
       />
+      <Helmet>
+        <link rel="preload" as="image" href="/vision/group.png" />
+      </Helmet>
 
-      <div className="hidden md:block -mx-[25%] w-[150%]">
+      <div className="hidden md:block w-full min-[1500px]:-mx-[25%] min-[1500px]:w-[150%]">
         <img
           src="/vision/group.png"
           alt="A collage of digital and physical product touchpoints"
+          width={2600}
+          height={1456}
+          loading="eager"
+          fetchPriority="high"
+          decoding="async"
           draggable={false}
           className="w-full"
         />
